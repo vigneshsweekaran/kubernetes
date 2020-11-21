18a7583a867ba18b7c5e1db90ec85213bcfa399d
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-11-21 10:23:29 +0530

    Update setup.sh

--- play-with-k8s/setup.sh
+++ play-with-k8s/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# 1. Initializes cluster master node:
-kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
-
-# 2. Initialize cluster networking:
-kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
-
-# 3. Untaint master node to deploy pods in master node
-kubectl taint nodes --all node-role.kubernetes.io/master:NoSchedule-
-
-# 4. Installing Helm version 3
+# Installing Helm version 3
 export VERIFY_CHECKSUM=false
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-# 5. Configuring git username and mail
+# Configuring git username and mail
 git config user.email "play-with-kubernetes@gmail.com"
 git config user.name "play-with-kubernetes"
+
+# Initializes cluster master node:
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+
+# Initialize cluster networking:
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+
+# Untaint master node to deploy pods in master node
+# kubectl taint nodes --all node-role.kubernetes.io/master:NoSchedule-
